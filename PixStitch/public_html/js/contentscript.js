ba70bb75d07486c6919e55931636e769f9f9a2fd
Lise Ho <koprty@gmail.com>
2015-09-05 17:28:51 -0400

    GRRR JQUERY

--- PixStitch/public_html/js/contentscript.js
+++ PixStitch/public_html/js/contentscript.js
@@ -1,3 +1,19 @@
+function changeImageRandom(imgLink)
+{
+	var rand = Math.floor((Math.random() * $("img").length));
+	console.log(rand);
+ 	var image = $("img").get(rand).attr("src",imgLink);
+}
+function changeImageClick(element,imgLink)
+{
+
+	$("img").get(rand).attr("src",imgLink);
+}
+function changeImageAll(imgLink)
+{
+	console.log("YESS SUCCESS");
+	$("img").attr("src",imgLink);
+}
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(sender.tab ?
@@ -10,21 +26,7 @@ chrome.runtime.onMessage.addListener(
     //getting objects in message, use request.greeting
     if (request.greeting == "hello")
       sendResponse({farewell: "goodbye"});
+  	if (request.greeting == "something")
+  		changeImageRandom("https://s.zkcdn.net/Advertisers/bf6a160cde9746c890285a40334fe6c2.jpg");
   });
 
-
-function changeImageRandom(imgLink)
-{
-	var rand = (Math.Random() * $("img").length);
-	$("img").get(rand).attr("src",imLink);
-}
-function changeImageClick(element,imgLink)
-{
-
-	$("img").get(rand).attr("src",imgLink);
-}
-function changeImageAll(imgLink)
-{
-	console.log("YESS SUCCESS");
-	$("img").attr("src",imgLink);
-}
