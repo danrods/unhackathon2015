4f8dd3fb39d0fa991fd08a14a703494f9aee6e12
Lise Ho <koprty@gmail.com>
2015-09-05 19:52:06 -0400

    aksdljfkladsjflak

--- PixStitch/public_html/js/popup.js
+++ PixStitch/public_html/js/popup.js
@@ -1,18 +1,19 @@
 $(document).ready( function(){
 
-	$(".images").each(function(){
-		$(this).addClass("WOOPEE")
-	});
-	$(".images").each(function(){
-		$(this).removeClass("WOOPEE");
+	$(".imgBtn img").each(function(){
+		$(this).on("click", function(){
+			console.log($(this));
+			$(this).toggleClass(" WOOPEE");
+		});
 	});
 
 	var ImageList = {
-	/*hungry*/ "Hungry":"http://static.wixstatic.com/media/be8b00_96febcabacc147abad31999821d45c14.jpg_srz_960_960_85_22_0.50_1.20_0.00_jpg_srz",
-	/*Jesus*/ "Jesus":"https://gregorycrofford.files.wordpress.com/2014/08/eucharist.jpg",
+	/*hungry*/ "hungry":"http://static.wixstatic.com/media/be8b00_96febcabacc147abad31999821d45c14.jpg_srz_960_960_85_22_0.50_1.20_0.00_jpg_srz",
+	/*Jesus*/ "jesus":"https://gregorycrofford.files.wordpress.com/2014/08/eucharist.jpg",
 	/*MAD*/ "flower": "https://external.fphl1-1.fna.fbcdn.net/safe_image.php?d=AQBmvgwX5E6eQNHh&w=960&h=960&url=http%3A%2F%2Fwww.scaryforkids.com%2Fpics%2Fclown-movies.jpg",
 	/*Lizard*/ "lizard" :"http://cliparts.co/cliparts/di4/orx/di4orx9AT.jpg"
 			}
+	var sub = ImageList[$(".WOOPEE")[0]];
 
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   		chrome.tabs.sendMessage(tabs[0].id, {ID: "hello", imageLink:ImageList["Hungry"]}, function(response) {
