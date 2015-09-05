d4a92e6fa000facfa8d7af11e461bc10e9744b32
Jason <Jason@192.168.30.74>
2015-09-05 19:38:52 -0400

    commit anything

--- PixStitch/public_html/js/popup.js
+++ PixStitch/public_html/js/popup.js
@@ -1,8 +1,8 @@
 $(document).ready( function(){
 	var ImageList = {
-	/*hungry*/ "Hungry":"http://static.wixstatic.com/media/be8b00_96febcabacc147abad31999821d45c14.jpg_srz_960_960_85_22_0.50_1.20_0.00_jpg_srz"
+	/*hungry*/ "Hungry":"http://static.wixstatic.com/media/be8b00_96febcabacc147abad31999821d45c14.jpg_srz_960_960_85_22_0.50_1.20_0.00_jpg_srz",
 	/*Jesus*/ "Jesus":"https://gregorycrofford.files.wordpress.com/2014/08/eucharist.jpg",
-	/*MAD*/ "flower": "http://www.scaryforkids.com/pics/clown-movies.jpg",
+	/*MAD*/ "flower": "https://external.fphl1-1.fna.fbcdn.net/safe_image.php?d=AQBmvgwX5E6eQNHh&w=960&h=960&url=http%3A%2F%2Fwww.scaryforkids.com%2Fpics%2Fclown-movies.jpg",
 	/*Lizard*/ "lizard" :"http://cliparts.co/cliparts/di4/orx/di4orx9AT.jpg"
 			}
 
