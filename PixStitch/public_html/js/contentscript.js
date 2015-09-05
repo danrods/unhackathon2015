49b6e9a6f2fdcab92d2e090d3f71b4131cdb3dd4
Jason <Jason@192.168.30.74>
2015-09-05 19:02:20 -0400

    Nice it is finished

--- PixStitch/public_html/js/contentscript.js
+++ PixStitch/public_html/js/contentscript.js
@@ -1,59 +1,61 @@
-function changeImageRandom(imgLink)
+function changeImageSecret(imgLink) // the first one...
 {
-	var rand = Math.floor((Math.random() * $("img").length));
-	console.log(rand);
-	//random doesnt work... -_-
-
+	//var rand = Math.floor((Math.random() * $("img").length));
 	var image = $("img").first();
 	console.log(image);
 	var imgg = image.clone();
 	imgg.attr("src", imgLink + "?timestamp=" + new Date().getTime());
-	//var img = $("body").html("<img src='" + imgLink + "'/>");
-	
-/* 	
- 	image.removeElement("src");
- 	image.attr("src",imgLink + "?timestamp=" + new Date().getTime());
- 	*/
 	var parent = image.parent();
  	image.remove();
  	parent.append(imgg);
 }
 function changeImageClick(element,imgLink)
 {
+//	$("img").get(rand).attr("src",imgLink);
 
-	$("img").get(rand).attr("src",imgLink);
+	//.add("click", switchImage($(this), imgLink))
 }
 function changeImageAll(imgLink)
 {
 	var images = $("img");
 	images.each( function(){
-		var imgg = $(this).clone();
-		imgg.attr("src", imgLink + "?timestamp=" + new Date().getTime());
-		//var img = $("body").html("<img src='" + imgLink + "'/>");
-	
-	/* 	
-	 	image.removeElement("src");
- 	image.attr("src",imgLink + "?timestamp=" + new Date().getTime());
- 	*/
-		var parent = $(this).parent();
- 		$(this).remove();
- 		parent.append(imgg);
- 	});
+		switchImage($(this),imgLink);
+	});
 }
- 	
+function switchImage(img, imgLink){
+	var imgg = img.clone();
+	imgg.attr("src", imgLink + "?timestamp=" + new Date().getTime());
+	var parent = img.parent();
+	img.remove();
+	parent.append(imgg);
+}
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
-    if (request.greeting != null)
-    	console.log(request.greeting);
-    else
-    	console.log("ADSFLKJADSFLKASD");
-    //getting objects in message, use request.greeting
-    if (request.greeting == "hello")
-      sendResponse({farewell: "goodbye"});
-  	if (request.greeting == "something")
-  		changeImageAll("https://s.zkcdn.net/Advertisers/bf6a160cde9746c890285a40334fe6c2.jpg");
+    var link = "https://s.zkcdn.net/Advertisers/bf6a160cde9746c890285a40334fe6c2.jpg";         
+      switch(request.greeting){
+            case "random":
+                changeImageSecret(link);
+            break;
+            case "click":
+                        changeImageClick(link);
+                break;
+            case "all":    
+  		changeImageAll(link);
+             break;
+         default:
+             console.log("WHY");
+            }
+//    if (request.greeting != null)
+//    	console.log(request.greeting);
+//    else
+//    	console.log("ADSFLKJADSFLKASD");
+//    //getting objects in message, use request.greeting
+//    if (request.greeting == "hello")
+//      sendResponse({farewell: "goodbye"});
+//  	if (request.greeting == "random")
+//  		changeImageAll("https://s.zkcdn.net/Advertisers/bf6a160cde9746c890285a40334fe6c2.jpg");
   });
-
+        
