8cf1e7c2b6b2940bcfd8aa17336e1a9cbb018e74
Lise Ho <koprty@gmail.com>
2015-09-05 19:27:52 -0400

    edited stuff

--- PixStitch/public_html/js/popup.js
+++ PixStitch/public_html/js/popup.js
@@ -3,11 +3,10 @@ $(document).ready( function(){
 	/*hungry*/ "Hungry":"http://static.wixstatic.com/media/be8b00_96febcabacc147abad31999821d45c14.jpg_srz_960_960_85_22_0.50_1.20_0.00_jpg_srz"
 	/*Jesus*/ "Jesus":"https://gregorycrofford.files.wordpress.com/2014/08/eucharist.jpg",
 	/*MAD*/ "flower": "http://www.scaryforkids.com/pics/clown-movies.jpg",
-	/*Lizard*/ "lizard" :""
+	/*Lizard*/ "lizard" :"http://cliparts.co/cliparts/di4/orx/di4orx9AT.jpg"
 			}
 
-			ImageList["Hungry"]
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	  		chrome.tabs.sendMessage(tabs[0].id, {ID: "hello", imageLink:ImageList["Hungry"]}, function(response) {
 	  			if (response != null || response  != undefined)
 	    			console.log(response.farewell);
